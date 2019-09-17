f7131677db1a5404dac06780d75ab5123de2055f
Rafa <rafaelaylln@gmail.com>
2019-09-17 14:22:42 +0200

    First Upload

---
+++ Physics Training/Assets/Scrips/CircularMoving.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMoving : MonoBehaviour
+{
+
+    public Transform center;
+    public float angle = 0;
+    public Vector3 offset, distancia;
+    Rigidbody playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        center = GameObject.FindGameObjectWithTag("Finish").transform;
+        playerRigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia = center.position - transform.position;
+        playerRigidbody.AddForce(distancia);
+        playerRigidbody.AddForce(new Vector3(-distancia.y, distancia.x, 0));
+    }
+}
